4941f7fe8e9841bfd8004c658ac697b0b456fbbd
rahul_lohra <tgunix@gmail.com>
2022-08-13 01:59:42 +0530

    add scroll listener

--- app/src/main/java/com/rahul/notificationstest/feature/search/ui/fragments/UnsplashHomeFragment.kt
+++ app/src/main/java/com/rahul/notificationstest/feature/search/ui/fragments/UnsplashHomeFragment.kt
@@ -1,5 +1,6 @@
 package com.rahul.notificationstest.feature.search.ui.fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,19 +8,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.colorResource
@@ -30,20 +42,25 @@ import androidx.fragment.app.Fragment
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.rahul.notificationstest.App
 import com.rahul.notificationstest.R
-import com.rahul.notificationstest.di.component.AppComponent
 import com.rahul.notificationstest.feature.search.data.datasource.DummyDataProvider
 import com.rahul.notificationstest.feature.search.di.components.DaggerSearchComponent
 import com.rahul.notificationstest.feature.search.ui.viewmodels.SearchViewModel
 import com.rahul.notificationstest.ui.theme.typography
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 class UnsplashHomeFragment : Fragment() {
 
     @Inject
     lateinit var searchViewModel: SearchViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -114,8 +131,8 @@ fun HomeToolbar() {
 fun HomeScreen(modifier: Modifier = Modifier, photosFlow: Flow<PagingData<String>>) {
     Column {
         Header()
-        WeekdaysList()
-        PhotosDisplayList(photosFlow = photosFlow)
+        UnsplashViewPager(photosFlow)
+//        PhotosDisplayList(photosFlow = photosFlow)
     }
 }
 
@@ -152,27 +169,67 @@ fun WeekdaysList() {
     })
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun PhotosDisplayList(photosFlow: Flow<PagingData<String>>) {
     val dataList = photosFlow.collectAsLazyPagingItems()
     val maxColumnSpan = 2
     val minColumnSpan = 1
-    LazyVerticalGrid(columns = GridCells.Fixed(maxColumnSpan), content = {
-        items(
-            dataList.itemCount,
-            span = { index ->
-                if (index % 3 == 0) {
-                    GridItemSpan(maxColumnSpan)
-                } else {
-                    GridItemSpan(minColumnSpan)
-                }
-            },
-            itemContent = { index -> PhotosListItem(url = dataList[index]!!) }) //TODO Rahul why force null?
-    })
+    val coroutineScope = rememberCoroutineScope()
+    val gridState = rememberLazyGridState()
+    coroutineScope.launch {
+
+        gridState.scroll {
+            val endOffset = gridState.layoutInfo.viewportEndOffset
+            val startOffset = gridState.layoutInfo.viewportStartOffset
+            Timber.d("startOffset = $startOffset, endOffset = $endOffset")
+        }
+    }
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val delta = available.y
+
+                Timber.d("delta Y = ${delta}, index = ${gridState.firstVisibleItemIndex}, offset = ${gridState.firstVisibleItemScrollOffset}")
+                return Offset.Zero
+            }
+        }
+    }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(maxColumnSpan),
+        modifier = Modifier.nestedScroll(nestedScrollConnection),
+        state = gridState,
+        content = {
+            items(
+                dataList.itemCount,
+                span = { index ->
+                    if (index % 3 == 0) {
+                        GridItemSpan(maxColumnSpan)
+                    } else {
+                        GridItemSpan(minColumnSpan)
+                    }
+                },
+                itemContent = { index -> PhotosListItem(url = dataList[index]!!) }) //TODO Rahul why force null?
+        })
 }
 
 
 @Composable
 fun PhotosListItem(url: String) {
-    AsyncImage(model = url, contentDescription = null)
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        modifier = Modifier.height(300.dp),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun UnsplashViewPager(photosFlow: Flow<PagingData<String>>) {
+    val pagerState = rememberPagerState()
+    HorizontalPager(count = 2, state = pagerState) {
+        PhotosDisplayList(photosFlow = photosFlow)
+    }
 }
